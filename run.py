9a9e532d50222b16b030763d533bef22d53f18b3
Aurora <aurorastorm22@proton.me>
2023-04-21 16:21:58 +0000

    Add function for room three and content for both room two and three

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def intro():
         answer = input("> ")
         if answer == "1":
             print("\nSleep well.\n\n")
-            time.sleep(3)
+            # time.sleep(3)
             room_one()
             break
         else:
@@ -45,20 +45,68 @@ def room_one():
     print("\nWhat do you do?\n")
 
     print("1. Feel the walls; there must be something to stop this!\n")
-    print("2. Pinch your arm, certain you must be dreaming, and this will all pass. Right?")
+    print("2. Pinch your arm, certain you must be dreaming, and this will all pass. Right?\n")
 
     while True:
         answer = input("> ")
         if answer == "1":
-            print("One")
+            print("\nYou frantically search the walls and stumble upon an odd tile. \n")
             # time.sleep(3)
+            print("Pushing it, the hissing stops and a slit in the wall reveals itself.\n")
+            # time.sleep(3)
+            print("You throw yourself out of the space and land on a plush red carpet, coughing and wheezing.\n")
+            # time.sleep(3)
+            room_two()
             break
         elif answer == "2":
-            print("Two")
+            print("\nIt's not a dream, you slowly realize as the gas fills your lungs further, and you slip off into sleep, never waking up again.")
+            # game_over()
             break
         else:
             print("Incorrect value")
             print("Please, enter a number corresponding with your choice")
 
+
+def room_two():
+    print("Looking around, you're in the middle of a long hallway, both ends leading to different directions.\n")
+    # time.sleep(3)
+    print("After finding your breath, you get up on your feet, quite sure this isn't a dream anymore.\n")
+    # time.sleep(3)
+    print("Your one thought is that where ever you are, you need to get out.\n")
+    # time.sleep(3)
+    print("To your right, the hallway leads to an impressive armor and two new directions.")
+    # time.sleep()
+    print("To your left, the hallways lead to a massive portrait and one new direction.")
+    print("\nWhat do you do?\n")
+
+    print("1. Go right toward the armor.\n")
+    print("2. Go left towards the portrait.\n")
+
+    while True:
+        answer = input("> ")
+        if answer == "1":
+            print("\n You approach the shiny armor and pause to examine it.\n")
+            # time.sleep(3)
+            print("It's medieval-looking, polished, and in good condition, wielding a sword in both hands.\n")
+            # time.sleep(3)
+            print('"Weird, but there\'s something for everyone," you conclude, and just as you are about to step away, the armor rustles.\n')
+            # time.sleep(3)
+            print("Before you get the chance to react, the sword comes down, its shiny blade the last thing you see.\n")
+            # game_over()
+            break
+        elif answer == "2":
+            print("\nYou approach the portrait of a proud-looking man with a bushy mustache and a bowler hat, staring at nothingness with empty eyes.\n")
+            # time.sleep(3)
+            print("Squinting, the portrait's eyes are, in fact, empty, as in cut out, and you step away with a shudder.\n")
+            # time.sleep(3)
+            print("Turning to leave, something flashes in those hollow eyes, but as you look again, there's nothing different.\n")
+            # time.sleep(3)
+            print("A weird feeling sits in your stomach as you proceed.\n")
+            # room_three()
+            break
+        else:
+            print("Incorrect value")
+            print("Please, enter a number corresponding with your choice")
+    
 
 intro()
