8fcf52aeae317425c3b995c89aa727ceb330e241
Aurora <aurorastorm22@proton.me>
2023-04-24 17:37:15 +0000

    Add docstrings to all functions in run.py

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # THE MURDER CASTLE
 
 This Python-based text based adventure game is loosely based on the history of H. H Holmes, a serial killer from America that allegedly clamed at least 27 lives in his [Murder Castle](https://www.history.com/topics/crime/murder-castle#section_2). 
+I first learned about this case by [The Poisoner's Cabinet podcast](https://www.youtube.com/watch?v=y90seA7T9GU).
 I've also taken inspiration from the [video game](https://en.wikipedia.org/wiki/The_Dark_Pictures_Anthology:_The_Devil_in_Me) "The Dark Pictures Anthology: The Devil in Me" by Supermassive Games.
 
 Throughout the game, there are multiple choices leading either to death or survival, and depending on what choices you make along the way, your chance of escaping the Murder Castle increases. Or decreases.
@@ -27,6 +28,9 @@ As a true crime enthusiast, the idea came naturally, and creating all the rooms 
 
 # UX
 
+## Target audience
+- Anyone with an interest in true crime who enjoy gaming
+
 ## User goals
 - To play and enjoy a text-based game
 - Understand how the game works at the start of the game
@@ -37,10 +41,27 @@ As a true crime enthusiast, the idea came naturally, and creating all the rooms 
 - Being able to replay the game with different outcomes
 
 ## As a website owner:
-- Being able to easily add more rooms to the game to keep it updated
+- I want to create a story that's fun to read 
+- I want to create a fun game with different endings and options to choose from
+- I want to be able to easily add more rooms to the game to keep it updated
 
 # Flowchart
 The outline of the game was made with the help of [Ludichart](https://www.lucidchart.com/pages/)
 
 ![Ludichart1](documentation/ludichart_1.PNG)
 ![Ludichart1](documentation/ludichart_2.PNG)
+
+# Features
+
+- The app is built with a total of 8 different rooms. 
+- There room 1-4 then 5a and 5b depending on where the player chooses to go from room 4, followed by room 6 and 7.
+- There's a total of 6 different endings depending on your choices
+
+
+# Features Left To Implement
+
+- Create a function that stores each ending the player have gotten, so that they can "collect" endings and get everyone, and adding a special price for getting every ending.
+
+# Testing
+
+## Validator testing
--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-"""
-Imports
-"""
+# imports
 import story
 import pyfiglet
 import time
@@ -10,15 +8,12 @@ import sys
 """
 Måndag:
 
-- Skriv string-statements/beskrivningar till alla funktioner
 - Skriv README
 - Skriv DEAD endings
 """
 
 
-"""
-Variables
-"""
+# Variables
 
 weapon = None
 attack = None
@@ -28,11 +23,18 @@ ending = None
 
 
 def clear_terminal():
+    """
+    Clears the terminal of text
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def punctuation_pause():
-    string = '.\n'
+    """
+    Adds 3 dots (...) where the paragraphs needs furter separation
+    to show the story is moving forward
+    """
+    string = '...\n'
     for char in string:
         sys.stdout.write(char)
         sys.stdout.flush()
@@ -40,6 +42,14 @@ def punctuation_pause():
 
 
 def start_game():
+    """
+    Starts the game off with an ASCII welcome message
+    where the player gets to descide whether they 
+    chose to start the game or not, also explaining
+    how picking choices work.
+    Chosing not to sends them back to the start where
+    they can chose to start the game.
+    """
     result_1 = pyfiglet.figlet_format("Welcome in")
     print(result_1)
     story.start_game()
@@ -62,6 +72,11 @@ def start_game():
 
 
 def welcome():
+    """
+    A welcome screen to further set the atmosphere of the game
+    as well as let the player enter their name, followed by
+    an ASCII title screen.
+    """
     clear_terminal()
     punctuation_pause()
 
@@ -76,13 +91,11 @@ def welcome():
     intro()
 
 
-"""
-Introduction to the game to get the player started
-with a general idea of what the game is about
-"""
-
-
 def intro():
+    """
+    Start up of the actual game, setting the premise
+    for what the game is about
+    """
     clear_terminal()
     story.intro_text()
     while True:
@@ -92,9 +105,6 @@ def intro():
             # time.sleep(3)
             room_one()
             break
-        if answer == "2":
-            room_four()
-            break
         else:
             story.incorrect()
 
@@ -103,14 +113,13 @@ def intro():
 # # " ROOM ONE "
 # # """"""""""""
 
-# """
-# Function for the first area of the game with dual choices.
-# Depending on what the player chooses, they will either
-# proceed with the game or it will end.
-# The player will then be given the option to restart it.
-# """
-
 def room_one():
+    """
+    Room one of the game, throwing the player into
+    the Murder Castle and giving their first set of choices.
+    The right choice will send them on to Room two, and the
+    wrong one ends the game.
+    """
     clear_terminal()
     story.room_one_text()
     while True:
@@ -134,6 +143,12 @@ def room_one():
 
 
 def room_two():
+    """
+    Room two of the game where exploration starts and the 
+    options for what's right and wrong get more unclear.
+    The right choice will send them on to Room three, and the
+    wrong one ends the game.
+    """
     punctuation_pause()
     story.room_two_text()
     while True:
@@ -156,14 +171,14 @@ def room_two():
 # # " ROOM THREE "
 # # """"""""""""""
 
-# """
-# Function for the third room of the game with dual choices.
-# Depending on what the player chooses, they will either
-# proceed with the game or it will end.
-# The player will then be given the option to restart it.
-# """
 
 def room_three():
+    """
+    Room three of the game, similar premise as room one and two,
+    thought here the player have more options.
+    One right choice will send them on to Room four, and the
+    other ones ends the game.
+    """
     punctuation_pause()
     story.room_three()
     while True:
@@ -192,17 +207,17 @@ def room_three():
 # # """"""""""""
 
 
-# """
-# Function for the fourth room of the game with multiple choices.
-# Depending on what the player chooses, they will either
-# proceed with the game or it will end.
-# The player will then be given the option to restart it.
-
-# One option will add a bonus in a later game area.
-# """
-
-
 def room_four():
+    """
+    Room four of the game where the player is 
+    introduced to 4 different choices.
+    If the player choses to rest, this will add 
+    a buff for later in the game where they'll avoid
+    getting injuried.
+
+    Here, the player will end up in either Room five a
+    or Room five b.
+    """
     punctuation_pause()
     story.room_four_text()
 
@@ -233,6 +248,12 @@ def room_four():
 
 
 def room_four_4():
+    """
+    The rest-option from Room four, from here 
+    the player get to choose from the same
+    options as previous, now with a rest buff 
+    added.
+    """
     global rest
     rest = True
 
@@ -267,6 +288,19 @@ def room_four_4():
 
 
 def room_five_a():
+    """
+    Room five a where the player will find one of two weapons
+    in the game, and gets to decide whether they want to pick 
+    it up or not. Depending on what they choose, it will give them
+    a different ending if they reach the end of the game.
+
+    No matter if they pick up the weapon or not, they'll move on
+    to Room six.
+
+    There's also a section of the room where the player will either
+    get injuried or not, depending on if they rested earlier, 
+    which will also affect the end of the game.
+    """
     global injury
     global weapon
     global attack
@@ -309,6 +343,19 @@ def room_five_a():
 
 
 def room_five_b():
+    """
+    Room five b where the player will find one of two weapons
+    in the game, and gets to decide whether they want to pick 
+    it up or not. Depending on what they choose, it will give them
+    a different ending if they reach the end of the game.
+
+    No matter if they pick up the weapon or not, they'll move on
+    to Room six.
+
+    There's also a section of the room where the player will either
+    get injuried or not, depending on if they rested earlier, 
+    which will also affect the end of the game.
+    """
     global injury
     global weapon
     global attack
@@ -359,9 +406,28 @@ def room_five_b():
 # # " ROOM SIX "
 # # """"""""""""""
 
+    """
+    Room five a where the player will find one of two weapons
+    in the game, and gets to decide whether they want to pick 
+    it up or not. Depending on what they choose, it will give them
+    a different ending if they reach the end of the game.
+
+    No matter if they pick up the weapon or not, they'll move on
+    to Room six.
+
+    There's also a section of the room where the player will either
+    get injuried or not, depending on if they rested earlier, 
+    which will also affect the end of the game.
+    """
 
 def room_six():
-
+    """
+    Room six of the game, same premise as previous rooms, with
+    two options leading to a new discovery, though one leads 
+    further to death.
+    One right choice will send them on to Room seven, and the
+    other ones ends the game.
+    """
     story.room_six()
 
     while True:
@@ -381,6 +447,15 @@ def room_six():
 
 
 def room_seven():
+    """
+    Final room of the game where the player will
+    either win or lose depending on their choices.
+    Here the injury and choice to pick up the weapon
+    or not plays a big part, and will decide the faith
+    of the player.
+
+    There are 6 different endings in total.
+    """
     global ending
 
     if injury is False and weapon is True:
@@ -421,26 +496,35 @@ def room_seven():
             else:
                 story.incorrect()
 
-    if injury is True and weapon is False:
-        clear_terminal()
-        story.room_seven_insta_death_injury()
-        ending = "6"
-        game_over()
-
     if injury is False and weapon is False:
         clear_terminal()
         story.room_seven_insta_death_no_injury()
         ending = "5"
         game_over()
 
+    if injury is True and weapon is False:
+        clear_terminal()
+        story.room_seven_insta_death_injury()
+        ending = "6"
+        game_over()
+
 
 def restart_game():
+    """
+    Function to restart the game whenever the
+    player dies. Gives the player the option to 
+    restart or not.
+    """
     story.restart_game()
     room_one()
 
 
 def win_game():
-    print(f"You got ending {ending} of 6\n")
+    """
+    Function for when the player wins the game, also offers
+    the option to restart and play another game.
+    """
+    # print(f"You got ending {ending} of 6\n")
     punctuation_pause()
     story.win_game()
 
@@ -455,10 +539,14 @@ def win_game():
 
 
 def game_over():
-    print(f"You got ending {ending} of 6")
+    """
+    Function for when the player dies, gives the option
+    for them to restart the game and will send them back
+    to Room one, otherwise they'll return to the startscreen.
+    """
     punctuation_pause()
     story.ask_to_restart_game()
-
+    # print(f"You got ending {ending} of 6\n")
     while True:
         answer = input("> ")
         if answer == "1":
